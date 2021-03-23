2e6152e00720d8805950443363535a55ec2a0d5f
Lars Kristan <lkristan@plume.com>
2021-03-23 20:17:43 +0100

    dock-run: Use SSH Agent in docker
    If SSH Agent is detected, mount the necessary files and set the
    SSH_AUTH_SOCK variable.
    
    Signed-off-by: Lars Kristan <lkristan@plume.com>

--- dock-run.sh
+++ dock-run.sh
@@ -2,8 +2,19 @@
 
 tag=$(echo ${PWD} | tr / - | cut -b2- | tr A-Z a-z)
 groups=$(id -G | xargs -n1 echo -n " --group-add ")
-params="-v ${PWD}:${PWD} --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd -v/etc/group:/etc/group ${tag}"
+params="-v ${PWD}:${PWD} --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd -v/etc/group:/etc/group"
+
+
+# If SSH Agent is detected, mount the necessary files and set the SSH_AUTH_SOCK variable
+[ -e "${SSH_AUTH_SOCK}" ] && {
+    params+=" -v/etc/ssl:/etc/ssl"
+    params+=" -v$(readlink -f ${SSH_AUTH_SOCK}):/ssh-agent -eSSH_AUTH_SOCK=/ssh-agent"
+    params+=" -v${HOME}/.ssh/known_hosts:${HOME}/.ssh/known_hosts"
+}
+
+# Append the tag after all options, but before the command
+params+=" ${tag}"
 
 docker build --tag=${tag} docker
 
-docker run $params $@
+docker run $params "$@"
