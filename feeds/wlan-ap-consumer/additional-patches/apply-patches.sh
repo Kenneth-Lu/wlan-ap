78f0710d581b9a8f3899b9fb6afd0706c9da6698
Miroslav Rajsek <mrajsek@plume.com>
2021-03-29 10:53:02 +0200

    wlan-ap-consumer: additional-patches minimisation
    For now removal of openwrt and leaving hostapd patches as they are.
    -- Removal of openwrt_feed_update.patch:
    openvswitch part steps:
    Copy openvswitch from openwrt/feeds/packages/net/openvswitch to wlan-ap-consumer.
    Adapt include paths to lang/python.
    Omit copying to /etc/init.d/openvswitch as per the patch.
    Remove the corresponding hunk from the patch.
    -----
    firewall part steps:
    Copy firewall from openwrt/package/network/config/firewall to wlan-ap-consumer.
    Add firewall to wlan-ap-consumer.yml packages.
    Omit copying to /etc/init.d/firewall as per the patch.
    Remove the corresponding hunk from the patch.
    -----
    dnsmasq part steps:
    Copy dnsmasq from openwrt/package/network/services/dnsmasq to wlan-ap-consumer.
    Omit copying to /etc/init.d/dnsmasq as per the patch.
    Remove the corresponding hunk from the patch.
    -----
    iproute2 part steps:
    Copy iproute2 from openwrt/package/network/utils/iproute2 to wlan-ap-consumer.
    Add iproute2 to wlan-ap-consumer.yml packages.
    Add copying ip as per the patch.
    Remove the corresponding hunk from the patch.
    -----
    After all the hunks deleted, modify apply-patches.sh accordingly.
    -- Removal of the fingerprint and vendor-class dnsmasq patches:
    Check that the two patches have been moved together with other dnsmasq
    patches, i.e. if apply-patches.sh had been launched before.
    Delete the two patches from additional-patches.

--- feeds/wlan-ap-consumer/additional-patches/apply-patches.sh
+++ feeds/wlan-ap-consumer/additional-patches/apply-patches.sh
@@ -2,16 +2,6 @@
 
 cd "$(dirname $0)"
 
-PATCH_PATH="../../../openwrt/"
-if [ ! -d "$PATCH_PATH" ]; then
-    echo "openwrt directory does not exist: $(dirname $0)/$PATCH_PATH"
-    exit 1
-fi
-patch -d "$PATCH_PATH" -p1 < "openwrt-feed-update.patch"
-echo
-echo "openwrt patched"
-echo
-
 FEED_PATH="../../../"
 if [ ! -d "$FEED_PATH" ]; then
     echo "path to add patches to does not exist: $(dirname $0)/$FEED_PATH"
